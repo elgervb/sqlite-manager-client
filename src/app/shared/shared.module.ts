5e2c0ab4c149753b2a5da919d42dd508d392cd4a
Elger van Boxtel <elger.van.boxtel@isatis-health.nl>
2017-05-17 16:59:32 +0200

    add spinner

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OffcanvasComponent } from './offcanvas/offcanvas.component';
-import {} from '';
+import { SpinnerComponent } from './spinner/spinner.component';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  exports: [OffcanvasComponent],
-  declarations: [OffcanvasComponent]
+  exports: [
+    OffcanvasComponent,
+    SpinnerComponent
+  ],
+  declarations: [OffcanvasComponent, SpinnerComponent]
 })
 export class SharedModule { }
