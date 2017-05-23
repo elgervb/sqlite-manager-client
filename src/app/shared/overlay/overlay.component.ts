fee79b6d0027a3e811243096fddde430b0a22972
Elger van Boxtel <elger.van.boxtel@isatis-health.nl>
2017-05-23 14:48:23 +0200

    add keyboard support

--- src/app/shared/overlay/overlay.component.ts
+++ src/app/shared/overlay/overlay.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-overlay',
@@ -14,13 +14,23 @@ export class OverlayComponent implements OnInit {
    * Any valid class name for in effect
    */
   @Input() effect: string;
-
+  /**
+   * Close overlay when pressing ESC key
+   */
+  @Input() escClose = true;
+  /**
+   * Callback for onClose
+   */
   @Output() onClose = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
     //
+  }
+
+  @HostListener('document:keyup.esc', ['$event']) onKeyUp($event: KeyboardEvent) {
+    this.close();
   }
 
   close(): void {
